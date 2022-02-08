d807c5d9df6a704bf81bd8d8c79ce86547cbfb2f
BB1464 <oluwafemioyedele908@gmail.com>
2022-02-08 14:47:07 +0100

    I added some codes to the leaflet maps, you can look at it from line 17 to 34.

--- Leaflet map.R
+++ Leaflet map.R
@@ -11,6 +11,28 @@ class(world)
   addTiles() %>% 
   addPolygons(group = 'name_long',label = ~name_long,highlight=highlightOptions(weight = 5,color ='red',fillOpacity = 0.7,bringToFront = TRUE))
 
+
+  
+  
+library(leaflet)
+library(spData)
+library(ggplot2)
+library(dplyr)
+  
+data("world")
+class(world)
+  
+  
+leaflet(world) %>% 
+addTiles() %>% 
+addPolygons(group = 'name_long',label = ~name_long,highlight=highlightOptions(weight = 5,color ='red',fillOpacity = 0.7,bringToFront = TRUE))
+  
+  
+# Second Approach  
+leaflet(world) %>% 
+addTiles() %>% 
+addPolygons(color='name_long',group = 'name_long',label = ~name_long,highlight=highlightOptions(weight = 5,color ='red',fillOpacity = 0.7,bringToFront = TRUE))
+  
   
 # Second Approach  
 leaflet(world) %>% 
